3df812dac05904fc6a1e7c5235af4ba19ae1d633
momortral <matthieu.taieb@orange.fr>
2019-05-19 11:26:57 -0500

    faux main

---
+++ main.py
@@ -0,0 +1,15 @@
+class Equipe:
+    nom = ""
+    stade = ""
+    entraineur = ""
+    nb_victoire = 0
+    nb_nul = 0
+    nb_defaite = 0
+    points = 0
+    buts_pour = 0
+    buts_contre = 0
+    ratio = 0
+    nb_carton_jaune = 0
+    nb_carton_rouge = 0
+
+    print("Partie finale: \n","Good: ")
